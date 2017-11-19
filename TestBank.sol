a02670b21d4436ad00a3e4a03a5dd84899b0cfdc
nichc74 <nichc74@gmail.com>
2017-11-19 13:25:26 -0800

    Add files via upload

--- TestBank.sol
+++ TestBank.sol
@@ -4,33 +4,51 @@ contract TestBank {
 
 	address owner;
 
+	//Example call -> TestBank.deployed().then(function(instance) { instance.getBalance(balances[0])})
+
 	// Somewhat like a hashtable of addresses/balances
 	mapping (address => uint) balances;
+
+	// Logs the transfer to blockchain
+	event Transfer(address indexed _from, address indexed _to, uint256 _value);
 
 	function TestBank(){
 		owner = msg.sender;
 
 		//tx.origin -> getting original caller's information. Owner can NOT be a contract
 		//msg.sender -> owner can be a contract
-		balances[msg.sender] = 1000;
+		balances[tx.origin] = 10000;
 	}
 
 	// Returns current balance in a specific account
-	function getBalance(address addr) returns(uint){
+	function getBalance(address addr) returns (uint) {
 		return balances[addr];
 	}
 
-	function deposit(uint value) returns(uint new_value) {
+	function deposit(address addr, uint value) returns(uint new_value) {
 		balances[msg.sender] += msg.value;
+		return balances[addr];
 	}
 
 	// Attempt to withdraw the given 'amount' of Ether from the account.
-    function withdraw(uint amount) {
+    function withdraw(address addr, uint amount) returns(uint new_value){
         // Skip if someone tries to withdraw 0 or if they don't have enough Ether to make the withdrawal.
         if (balances[msg.sender] < amount || amount == 0)
             return;
         balances[msg.sender] -= amount;
         msg.sender.send(amount);
+        return balances[addr];
+    }
+
+    // Does a transfer using the recievers address to send inputted amount 
+    function transfer(address receiver, uint amount) returns(bool sufficient) {
+    	if (balances[msg.sender] < amount) return false;
+    	balances[msg.sender] -= amount;
+    	balances[receiver] += amount;
+
+    	// Calls event transfer. 
+    	Transfer(msg.sender, receiver, amount);
+    	return true;
     }
 
     //remove contract from the block chain
