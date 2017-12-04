ccd31b852712b9af5467071a3ecf59e8577a6b47
David Liang <davidxliang@gmail.com>
2017-12-03 19:35:37 -0800

    finished test1 case

--- curProject/contracts/TokenTransaction.sol
+++ curProject/contracts/TokenTransaction.sol
@@ -34,14 +34,7 @@ contract TransactionFactory {
     function getBalance(bytes32 username) public constant returns(uint256) { 
         return (Accounts[username].enitBalance);
     }
-    function setBalance(address _username, uint256 amount, bool flag) public {
-        bytes32 username = getName(_username);
-        if (flag) {
-            Accounts[username].enitBalance += amount;
-        } else { 
-            Accounts[username].enitBalance -= amount;
-        }
-    }
+
     function getAddress(bytes32 username) public constant returns(address) { 
         return (Accounts[username].userAddress) ; 
     }
@@ -81,7 +74,7 @@ contract TokenTransaction is TransactionFactory {
     address public owner;
     // Log the transfer/action to blockchain 
     event Sold(address buyer, address seller, uint256 enitAmount, uint256 etherAmount);
-    event Brought(address buyer, address seller, uint256 enitAmount, uint256 etherAmount);
+    event Bought(address buyer, address seller, uint256 enitAmount, uint256 etherAmount);
     event Transferred(address _from, address _to, uint256 value);
     event Deposit(address sender, uint256 buyer);
     event BadAction(address buyer, address otherBuyer, uint256 amountCost);
@@ -93,6 +86,15 @@ contract TokenTransaction is TransactionFactory {
      */
     function TokenTransaction() TransactionFactory() public {
         owner = msg.sender;
+    }
+
+    function setBalance(address _username, uint256 amount, bool flag) private {
+        bytes32 username = getName(_username);
+        if (flag) {
+            Accounts[username].enitBalance += amount;
+        } else { 
+            Accounts[username].enitBalance -= amount;
+        }
     }
 
     /* Purpose: converts depending on boolean:
@@ -118,14 +120,15 @@ contract TokenTransaction is TransactionFactory {
         setBalance(getAddress(name), value, true);
         Deposit(getAddress(name), value);
     }
+
     /* Purpose: transfers ether to seller and enit to buyer
      * Input: string_seller, uint256_amount 
      * Returns: bool
      */
-    function transferEnergy(uint256 amount) payable public returns(bool) {
-        (msg.sender).transfer(amount);
-        return true; 
-    }
+    // function transferEnergy(uint256 amount) payable public returns(bool) {
+    //     (msg.sender).transfer(amount);
+    //     return true; 
+    // }
 
     /* Purpose: exchange money or a handshake here 
                 buyer wants to buy energy from seller
@@ -153,14 +156,14 @@ contract TokenTransaction is TransactionFactory {
     //     return true;
     // }
 
-    function exchange1(bytes32 _buyer, bytes32 _seller, uint amount) public returns(bool) {
+    function exchange1(bytes32 _buyer, bytes32 _seller, uint amount) private returns(bool) {
         var seller = getAddress(_seller);
         var buyer = getAddress(_buyer);
         uint256 currentCost = convertToken(false, amount);
         Sold(buyer, seller, amount, currentCost); //give me the event log of this event
         
         transferEnergy1(_buyer, _seller, amount); 
-        Brought(buyer, seller, amount, currentCost);
+        Bought(buyer, seller, amount, currentCost);
         return true;
     }
     function transferEnergy1(bytes32 buyer, bytes32 seller, uint256 amount) public returns(bool) {
--- curProject/test/test.js
+++ curProject/test/test.js
@@ -17,13 +17,31 @@ contract('trans', function(accounts) {
             instance.register(accounts[3], "Kevin")            
         }).then(function() { 
             console.log("here");
-            instance.depositEnits("David", 10).then(instance.getBalance("David").then(console.log));
+            instance.depositEnits("David", 10).then(instance.getBalance("David").log());
             var balance = instance.getBalance("David").call();
+            assert(balance == 10);
             console.log(balance);
             console.log("done");
+        }).then(function() { 
+            assert(instance.getBalance("David") > instance.getBalance("Nick"));  
         });
         // action.then(function(instance) { instance.isRegistered("kevin").then(console.log).then(console.log("checked"));});
   });
+  it("testing exchange here now", function() {
+      trans.deployed().then(function(instance) { 
+        instance.register(accounts[0], "David")
+        instance.register(accounts[1], "Nick")
+      }).then(function() { 
+        instance.depositEnits("David", 10)
+        instance.depositEnits("Nick", 20)
+        assert(instance.getBalance("David") == 10)
+        assert(instance.getBalance("Nick") == 20)
+      }).then(function() {
+          instance.transferEnergy1("David", "Nick", 5)
+          assert(getBalance("David") == 15)
+          assert(getBalance("Nick") == 15)
+      })
+  })
   it("done YEAH", function async() {
         console.log("yeah, end");
   });
