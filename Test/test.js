3450702ebc5c0f6228c19bc6f597abfd76adbb9c
David Liang <davidxliang@gmail.com>
2017-12-03 22:41:37 -0800

    finished unit testing

---
+++ Test/test.js
@@ -0,0 +1,56 @@
+var trans = artifacts.require("./TokenTransaction.sol");
+var factory = artifacts.require("TransactionFactory");
+contract('trans', function(accounts) {
+    it("should put in the first account", function() {
+        var action = trans.deployed();
+        action.then(function(instance) { instance.isRegistered("kevin").then(console.log("checked"));});
+  });
+});
+
+// Zack Olson 
+// testing module is TransactionFactory 
+// Equivalence classes are the names of the group members and on 
+// whatever or not they are registered as well as if they get values.
+contract('trans', function(accounts) {
+    it("testing 4 accounts", function() {
+        var action = trans.deployed();
+        action.then(function(instance) { 
+            instance.register(accounts[0], "David")
+            instance.register(accounts[1], "Nick")
+            instance.register(accounts[2], "Zack")
+            instance.register(accounts[3], "Kevin")            
+        }).then(function() { 
+            console.log("here");
+            instance.depositEnits("David", 10).then(instance.getBalance("David").log());
+            var balance = instance.getBalance("David").call();
+            assert.equal(balance, 10);
+            console.log(balance);
+            console.log("done");
+        }).then(function() { 
+            requires(instance.getBalance("David") > instance.getBalance("Nick"));  
+        });
+        // action.then(function(instance) { instance.isRegistered("kevin").then(console.log).then(console.log("checked"));});
+  });
+// David did this test case
+// module is TokenTransaction
+// in theory, this test case here has all of the equivalence classes 
+// for all of the groupmates.
+  it("testing exchange here now", function() {
+      trans.deployed().then(function(instance) { 
+        instance.register(accounts[0], "David")
+        instance.register(accounts[1], "Nick")
+      }).then(function() { 
+        instance.depositEnits("David", 10)
+        instance.depositEnits("Nick", 20)
+        requires(instance.getBalance("David") == 10)
+        requires(instance.getBalance("Nick") == 20)
+      }).then(function() {
+          instance.transferEnergy1("David", "Nick", 5)
+          requires(getBalance("David") == 15).log();
+          requires(getBalance("Nick") < 43298402938);
+      });
+  });
+  it("done YEAH", function async() {
+        console.log("yeah, end");
+  });
+});``
